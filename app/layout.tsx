d2082f3dfcf16ffc90f84be97e6c616b7f3402f4
mhaz <mhazmiee@gmail.com>
2026-05-07 07:45:27 +0700

    Change Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Providers } from "@/components/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nismara Logistics | Virtual Command Center",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" className="dark">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <body className={`${roboto.className} flex flex-col min-h-screen`}>
         {/* Navbar akan selalu ada di paling atas */}
         <Providers>
           <Navbar />
