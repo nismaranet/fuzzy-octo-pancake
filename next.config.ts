6286ee9ed8c1514be40d02cc76db9601efe86df0
mhaz <mhazmiee@gmail.com>
2026-05-07 07:44:42 +0700

    New Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Jika Anda menggunakan image dari domain tunnel
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.nismara.my.id",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.steamstatic.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dev.nismara.my.id",
+        pathname: "/**",
+      },
+    ],
+  },
+  // Izinkan tunnel untuk bypass proteksi host jika diperlukan
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["dev.nismara.my.id"],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
